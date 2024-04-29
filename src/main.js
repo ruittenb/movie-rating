bac5307005a191bef308cb2b32f3b57f03dde81f
René Uittenbogaard <r.uittenbogaard@wearebold.digital>
2024-04-29 16:35:41 +0200

    doc: removed unused comment

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import vClickOutside from 'click-outside-vue3'
 import '../style.css'
 
-//Vue.use(vClickOutside)
 const app = createApp(App)
 app.use(vClickOutside)
 app.mount('#app')
