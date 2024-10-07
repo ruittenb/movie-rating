e935d3980097702c137c118d7633e0047bcd0de1
René Uittenbogaard <ruittenb@gmail.com>
2024-10-07 13:50:22 +0200

    fix: prevent tailwind warnings about deprecation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      ...colors,
-      'star-gold': '#fbbf24',
-      'star-disabled': '#888'
-    },
+    extend: {
+      colors: {
+        'star-gold': '#fbbf24',
+        'star-disabled': '#888'
+      }
+    }
   },
   plugins: [require("@tailwindcss/typography")],
 };
