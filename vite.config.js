4d5ee88cfa79716d5f33b7d138e9570cb7e6fff4
René Uittenbogaard <ruittenb@gmail.com>
2025-04-23 22:42:52 +0200

    feat: added auto-import for vue exports

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue', // importeert automatisch ref, computed, reactive, etc.
+      ],
+      dts: 'src/types/auto-imports.d.ts',
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
